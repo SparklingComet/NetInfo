a1727b9a80e95e9ea90496734915cd15c4a4fb5b
Sparkling Comet <apps@shanerx.org>
2018-03-03 17:52:42 +0100

    Update request.php

--- request.php
+++ request.php
@@ -1,10 +1,10 @@
 <?php
 
 print '{';
-print '"remote_addr": "' . $_SERVER['REMOTE_ADDR'] . '",';
-print '"remote_host": "' . $_SERVER['REMOTE_HOST'] . '",';
-print '"remote_port": "' . $_SERVER['REMOTE_PORT'] . '",';
-print '"request_method": "' . $_SERVER['REQUEST_METHOD'] . '",';
-print '"request_time": "' . $_SERVER['REQUEST_TIME'] . '",';
-print '"request_uri": "' . $_SERVER['REQUEST_URI'] . '"';
+print '\n  "remote_addr": "' . $_SERVER['REMOTE_ADDR'] . '",';
+print '\n  "remote_host": "' . $_SERVER['REMOTE_HOST'] . '",';
+print '\n  "remote_port": "' . $_SERVER['REMOTE_PORT'] . '",';
+print '\n  "request_method": "' . $_SERVER['REQUEST_METHOD'] . '",';
+print '\n  "request_time": "' . $_SERVER['REQUEST_TIME'] . '",';
+print '\n  "request_uri": "' . $_SERVER['REQUEST_URI'] . '"';
 print '}';
