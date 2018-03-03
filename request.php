df31432350a077898d009e372a9997b6d8b9b61e
Sparkling Comet <apps@shanerx.org>
2018-03-03 18:00:31 +0100

    Format JSON

--- request.php
+++ request.php
@@ -1,10 +1,11 @@
 <?php
 
-print '{';
-print '\n  "remote_addr": "' . $_SERVER['REMOTE_ADDR'] . '",';
-print '\n  "remote_host": "' . $_SERVER['REMOTE_HOST'] . '",';
-print '\n  "remote_port": "' . $_SERVER['REMOTE_PORT'] . '",';
-print '\n  "request_method": "' . $_SERVER['REQUEST_METHOD'] . '",';
-print '\n  "request_time": "' . $_SERVER['REQUEST_TIME'] . '",';
-print '\n  "request_uri": "' . $_SERVER['REQUEST_URI'] . '"';
-print '}';
+$new_ln = "\n  ";
+print "{";
+print $new_ln . '"remote_addr": "' . $_SERVER['REMOTE_ADDR'] . '",';
+print $new_ln . '"remote_host": "' . $_SERVER['REMOTE_HOST'] . '",';
+print $new_ln . '"remote_port": "' . $_SERVER['REMOTE_PORT'] . '",';
+print $new_ln . '"request_method": "' . $_SERVER['REQUEST_METHOD'] . '",';
+print $new_ln . '"request_time": "' . $_SERVER['REQUEST_TIME'] . '",';
+print $new_ln . '"request_uri": "' . $_SERVER['REQUEST_URI'] . '"';
+print "\n}";
